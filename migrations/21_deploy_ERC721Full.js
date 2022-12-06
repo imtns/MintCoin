e0fd78c7bbc3f970d7f3695548b8748be9167221
imtns <huangxin@alwaysnb.com>
2022-12-06 12:40:58 +0800

    init

---
+++ migrations/21_deploy_ERC721Full.js
@@ -0,0 +1,8 @@
+//固定总量代币
+const ERC721FullContract = artifacts.require("ERC721FullContract"); 
+
+module.exports = function(deployer) {
+    deployer.deploy(ERC721FullContract,
+    //构造函数的参数
+    "My Game Token","MGT",'https://github.com/Fankouzu/MintCoin/blob/master/');
+};
